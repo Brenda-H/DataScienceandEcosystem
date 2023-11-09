4ac8d2abbe330b7fe569fe2ebac657e6bf399bbe
Brenda-H <54257119+Brenda-H@users.noreply.github.com>
2023-11-09 15:38:16 +0700

    DataScience and Ecosystem by Khine Hnin Nwe
    This python file includes the  tutorial code and outputs of IBM Juypter Notebook course.

---
+++ DataScience and Ecosystem.py
@@ -0,0 +1,58 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem
+# 
+# ### In this notebook , Data Science Tools and Ecosystems are summarized.
+# 
+# Some of the popular languages that Data Scientists use are:
+# 
+# 1. Python
+# 2. R
+# 3. Java
+# 4. C++
+# 5. Julia
+# 
+# |Data Science Tools|
+# -----------------------------
+# |Jupyter Notebook|
+# |Rstudio|
+# |Anaconda Navigator|
+# |GitLab|
+# 
+# ### Below are a few examples of evaluating arithmetic expressions in Python.
+# 
+# 
+
+# In[1]:
+
+
+#This a simple arithmetic expression to mutiply then add integers
+
+a= (3*4)+5
+print (a)
+
+
+# In[2]:
+
+
+#This will convert 200 minutes to hours by diving by 60
+mins= 60
+hours=200/mins
+print (hours)
+
+
+# **Objectives**
+# - Unordered List
+#      * Julia
+#      * Python
+#      * R
+
+# ## Author
+# Khine Hnin Nwe
+
+# In[ ]:
+
+
+
+
